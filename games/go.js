fb84192584e362f9b3b1e2a46c1c4466c043a8c5
Jacob Parker <j@cob.xxx>
2012-12-28 15:29:13 -0500

    fixed bad check in creategame

--- games/go.js
+++ games/go.js
@@ -37,6 +37,9 @@ function createGameHandler(req, res) {
 
   server.users.getUserFromUID(uid, function(e, user) {
     if(e) {
+      server.unexpected(res, 'create game handler', 'could not get user from uid: ' + e.message);
+    } if(user == null) {
+      res.writeHead(400, {'Content-Type': 'text/plain'});
       res.end('Bad opponent uid.');
     } else {
       createGame(req.session.user, user, res);
